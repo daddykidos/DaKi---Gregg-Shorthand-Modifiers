feaaf1e323703df44d18f837f39d469c157e83b0
DaKi <89294381+daddykidos@users.noreply.github.com>
2026-01-17 13:00:28 +0800

    Initial upload

---
+++ index.js
@@ -0,0 +1,18 @@
+function id(a) {
+    return document.getElementById(a)
+}
+
+function goTo(a){
+    a = id(a)
+window.scrollTo(0,a.offsetTop)
+}
+
+//quick go to function (since idk how to hyperlink)
+let buttons = ['intro1','altsymbols','altchars','afterword','helpful']
+
+buttons.forEach(element => {
+    document.getElementById(element+'link').onclick = (e)=>{
+    goTo(element)
+}
+});
+

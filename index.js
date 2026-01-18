f94ce5511627965dc4e0c229495b78a05c94cae2
DaKi <89294381+daddykidos@users.noreply.github.com>
2026-01-18 20:11:11 +0800

    Added Custom briefs/neaten directory

--- index.js
+++ index.js
@@ -8,11 +8,14 @@ window.scrollTo(0,a.offsetTop)
 }
 
 //quick go to function (since idk how to hyperlink)
-let buttons = ['intro1','altsymbols','altchars','afterword','helpful']
+let buttons = ['intro1','altsymbols','altchars','afterword','helpful','briefs']
 
 buttons.forEach(element => {
+    
     document.getElementById(element+'link').onclick = (e)=>{
+        if (element!='briefs'){
     goTo(element)
+        }else {window.open('briefs.html')}
 }
 });
 
